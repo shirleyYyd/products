a6c720f609dc3e71d3db56f809dcd56aaaa94341
shirley Yang <yangyudi19880609@126.com>
2022-05-21 21:58:39 +0800

    add read file

--- products.py
+++ products.py
@@ -1,5 +1,18 @@
 products = []
 
+# 讀取檔案
+with open('products.csv', 'r') as f:
+	for line in f:
+		# 想要跳過欄位名稱的字串
+		if '商品, 價格' in line:
+			#都只能在迴圈裡 break-跳出迴圈 ，continue-跳掉下一迴
+			continue
+		# 字串後面有'\n'，使用stirp()出掉換行符號（\n）
+		name, price = line.strip().split(',') #split 切割 
+		products.append([name, price])
+print(products)
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
@@ -22,12 +35,13 @@ while True:
 	# 第3中
 	products.append([name, price])
 	
-	
+
+# 印出所有購買記錄	
 print(products)
 # 存取二維list
 print(products[0][0])
 
-
+# 寫入檔案
 for p in products: 
 	print(p)# 列印出清單中每一個小清單
 	print(p[0]) # 每一個小清單的第0個index
