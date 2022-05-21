1e7c5780c63f1e506e95f03857835e95fdce7b94
shirley Yang <yangyudi19880609@126.com>
2022-05-21 21:20:30 +0800

    add header

--- products.py
+++ products.py
@@ -4,7 +4,7 @@ while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
 		break
-	price = input('請輸入商品價格： ')
+	price = int(input('請輸入商品價格： ') )
 	p = []
 
 	"""
@@ -32,3 +32,9 @@ for p in products:
 	print(p)# 列印出清單中每一個小清單
 	print(p[0]) # 每一個小清單的第0個index
 	print(p[0], '的價格是', p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品, 價格\n')#加欄位名稱
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n' ) #4個字串合併在一起為一個字串
+		#csv檔若沒有,區隔會變成1格
